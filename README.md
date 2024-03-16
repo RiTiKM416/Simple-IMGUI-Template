8426fbafac9918078b1d6703ff0ffd99e5500eee
Ritik Meshram <160781342+RiTiKM416@users.noreply.github.com>
2024-03-16 20:15:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿
+## 🌌 Setup
+- Clone the repo
+- Customize menu to your liking
+- Build in Release | x86
+- Find your binary in the `Release` folder
+- Enjoy
+
+## 🗿 Disclaimer
+I am not responsible for anything that happens when you use this software. Thank You.
---
+++ cheat/gui.cpp
@@ -0,0 +1,315 @@
+#include "gui.h"
+#include <iostream>
+#include <Windows.h>
+#include <shellapi.h>
+#include <stdio.h>
+
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_dx9.h"
+#include "../imgui/imgui_impl_win32.h"
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(
+	HWND window,
+	UINT message,
+	WPARAM wideParameter,
+	LPARAM longParameter
+);
+
+long __stdcall WindowProcess(
+	HWND window,
+	UINT message,
+	WPARAM wideParameter,
+	LPARAM longParameter)
+{
+	if (ImGui_ImplWin32_WndProcHandler(window, message, wideParameter, longParameter))
+		return true;
+
+	switch (message)
+	{
+	case WM_SIZE: {
+		if (gui::device && wideParameter != SIZE_MINIMIZED)
+		{
+			gui::presentParameters.BackBufferWidth = LOWORD(longParameter);
+			gui::presentParameters.BackBufferHeight = HIWORD(longParameter);
+			gui::ResetDevice();
+		}
+	}return 0;
+
+	case WM_SYSCOMMAND: {
+		if ((wideParameter & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+			return 0;
+	}break;
+
+	case WM_DESTROY: {
+		PostQuitMessage(0);
+	}return 0;
+
+	case WM_LBUTTONDOWN: {
+		gui::position = MAKEPOINTS(longParameter); // set click points
+	}return 0;
+
+	case WM_MOUSEMOVE: {
+		if (wideParameter == MK_LBUTTON)
+		{
+			const auto points = MAKEPOINTS(longParameter);
+			auto rect = ::RECT{ };
+
+			GetWindowRect(gui::window, &rect);
+
+			rect.left += points.x - gui::position.x;
+			rect.top += points.y - gui::position.y;
+
+			if (gui::position.x >= 0 &&
+				gui::position.x <= gui::WIDTH &&
+				gui::position.y >= 0 && gui::position.y <= 19)
+				SetWindowPos(
+					gui::window,
+					HWND_TOPMOST,
+					rect.left,
+					rect.top,
+					0, 0,
+					SWP_SHOWWINDOW | SWP_NOSIZE | SWP_NOZORDER
+				);
+		}
+
+	}return 0;
+
+	}
+
+	return DefWindowProc(window, message, wideParameter, longParameter);
+}
+
+void gui::CreateHWindow(const char* windowName) noexcept
+{
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_CLASSDC;
+	windowClass.lpfnWndProc = WindowProcess;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = GetModuleHandleA(0);
+	windowClass.hIcon = 0;
+	windowClass.hCursor = 0;
+	windowClass.hbrBackground = 0;
+	windowClass.lpszMenuName = 0;
+	windowClass.lpszClassName = "class001";
+	windowClass.hIconSm = 0;
+
+	RegisterClassEx(&windowClass);
+
+	window = CreateWindowEx(
+		0,
+		"class001",
+		windowName,
+		WS_POPUP,
+		100,
+		100,
+		WIDTH,
+		HEIGHT,
+		0,
+		0,
+		windowClass.hInstance,
+		0
+	);
+
+	ShowWindow(window, SW_SHOWDEFAULT);
+	UpdateWindow(window);
+}
+
+void gui::DestroyHWindow() noexcept
+{
+	DestroyWindow(window);
+	UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+}
+
+bool gui::CreateDevice() noexcept
+{
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+	if (!d3d)
+		return false;
+
+	ZeroMemory(&presentParameters, sizeof(presentParameters));
+
+	presentParameters.Windowed = TRUE;
+	presentParameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	presentParameters.BackBufferFormat = D3DFMT_UNKNOWN;
+	presentParameters.EnableAutoDepthStencil = TRUE;
+	presentParameters.AutoDepthStencilFormat = D3DFMT_D16;
+	presentParameters.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
+
+	if (d3d->CreateDevice(
+		D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_HAL,
+		window,
+		D3DCREATE_HARDWARE_VERTEXPROCESSING,
+		&presentParameters,
+		&device) < 0)
+		return false;
+
+	return true;
+}
+
+void gui::ResetDevice() noexcept
+{
+	ImGui_ImplDX9_InvalidateDeviceObjects();
+
+	const auto result = device->Reset(&presentParameters);
+
+	if (result == D3DERR_INVALIDCALL)
+		IM_ASSERT(0);
+
+	ImGui_ImplDX9_CreateDeviceObjects();
+}
+
+void gui::DestroyDevice() noexcept
+{
+	if (device)
+	{
+		device->Release();
+		device = nullptr;
+	}
+
+	if (d3d)
+	{
+		d3d->Release();
+		d3d = nullptr;
+	}
+}
+
+void gui::CreateImGui() noexcept
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ::ImGui::GetIO();
+
+	io.IniFilename = NULL;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(device);
+}
+
+void gui::DestroyImGui() noexcept
+{
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+}
+
+void gui::BeginRender() noexcept
+{
+	MSG message;
+	while (PeekMessage(&message, 0, 0, 0, PM_REMOVE))
+	{
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+
+		if (message.message == WM_QUIT)
+		{
+			isRunning = !isRunning;
+			return;
+		}
+	}
+
+	// Start the Dear ImGui frame
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+}
+
+void gui::EndRender() noexcept
+{
+	ImGui::EndFrame();
+
+	device->SetRenderState(D3DRS_ZENABLE, FALSE);
+	device->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+	device->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+
+	device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_RGBA(0, 0, 0, 255), 1.0f, 0);
+
+	if (device->BeginScene() >= 0)
+	{
+		ImGui::Render();
+		ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+		device->EndScene();
+	}
+
+	const auto result = device->Present(0, 0, 0, 0);
+
+	// Handle loss of D3D9 device
+	if (result == D3DERR_DEVICELOST && device->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+		ResetDevice();
+}
+
+void gui::Render() noexcept
+{
+    ImGui::SetNextWindowPos({ 0, 0 });
+    ImGui::SetNextWindowSize({ WIDTH, HEIGHT });
+    ImGui::Begin(
+        "BillA iz GOD or WHOT",
+        &isRunning,
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoSavedSettings |
+        ImGuiWindowFlags_NoCollapse |
+        ImGuiWindowFlags_NoMove
+    );
+
+    if (ImGui::Button("YouTube", ImVec2{ 100, 50 }))
+    {
+        ShellExecute(NULL, "open", "https://www.youtube.com/@ritikm416", 0, 0, SW_SHOWNORMAL);
+    }
+
+    if (ImGui::Button("DonateMoney", ImVec2{ 100, 50 }))
+    {
+        // Handle DonateMoney button click action here
+    }
+
+    // Define the button size
+    ImVec2 buttonSize = ImVec2(100, 30);
+
+    // Struct to store the buttons info
+    struct MyButton {
+        const char* label;
+        const char* url;
+        const char* imageUrl;
+    };
+
+    // array of button info
+    MyButton buttons[] = {
+        { "Discord", "https://discord.gg/xenostore", "https://cdn.discordapp.com/attachments/1182635052467757120/1216841533253488701/github-logo.png?ex=6601dafd&is=65ef65fd&hm=861f297575cb0fc6f9b93db0cb34545702ac7713240b449e3cb7ca9d330572a1&" },
+        { "GitHub", "https://github.com/RiTiKM416/ritikm416", "" },
+        { "Instagram", "https://www.instagram.com/ani.xd_00/", "" }
+    };
+
+    // Create a new column layout
+    ImGui::Columns(1);
+
+    // Create a button for each URL with the same size
+    for (int i = 0; i < IM_ARRAYSIZE(buttons); i++)
+    {
+        if (!buttons[i].imageUrl.empty())
+        {
+            // Load the image
+            ImVec2 imageSize = ImVec2(buttonSize.x, buttonSize.y);
+            ImGui::Image((ImTextureID)ImGui::GetImage(buttons[i].imageUrl), imageSize);
+
+            // Handle the button click action
+            if (ImGui::ButtonBehavior("", imageSize))
+            {
+                ShellExecute(NULL, "open", buttons[i].url, 0, 0, SW_SHOWNORMAL);
+            }
+        }
+        else
+        {
+            // Handle the button click action
+            if (ImGui::Button(buttons[i].label, buttonSize))
+            {
+                ShellExecute(NULL, "open", buttons[i].url, 0, 0, SW_SHOWNORMAL);
+            }
+        }
+        ImGui::NextColumn();
+    }
+
+    ImGui::End();
+}
